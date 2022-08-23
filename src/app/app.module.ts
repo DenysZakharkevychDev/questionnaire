de569d8f6a7ed69db4f718c68772ca2d6d67bb0d
DenysZakharkevychDev <denys.zakharkevych.dev@gmail.com>
2022-08-23 16:55:20 +0300

    added pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionManagementComponent } from './core/pages/question-management/question-management.component';
+import { CreateQuestionComponent } from './core/pages/create-question/create-question.component';
+import { EditQuestionComponent } from './core/pages/edit-question/edit-question.component';
+import { ListOfQuestionsComponent } from './core/pages/list-of-questions/list-of-questions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionManagementComponent,
+    CreateQuestionComponent,
+    EditQuestionComponent,
+    ListOfQuestionsComponent
   ],
   imports: [
     BrowserModule,
