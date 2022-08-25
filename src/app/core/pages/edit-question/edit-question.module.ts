b5dceeb07a686e4d6e93cde6f158daf00a02602e
DenysZakharkevychDev <denys.zakharkevych.dev@gmail.com>
2022-08-25 15:31:31 +0300

    edit page

--- src/app/core/pages/edit-question/edit-question.module.ts
+++ src/app/core/pages/edit-question/edit-question.module.ts
@@ -1,10 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { QuestionCardConstructorModule } from './../create-question/components/question-card-constructor/question-card-constructor.module';
 import { EditQuestionRoutingModule } from './edit-question-routing.module';
 import { EditQuestionComponent } from './edit-question.component';
 
 @NgModule({
   declarations: [EditQuestionComponent],
-  imports: [CommonModule, EditQuestionRoutingModule],
+  imports: [
+    CommonModule,
+    EditQuestionRoutingModule,
+    QuestionCardConstructorModule,
+  ],
 })
 export class EditQuestionModule {}
