e3a0357a280f73a5352fec81f3e279754459ab52
DenysZakharkevychDev <denys.zakharkevych.dev@gmail.com>
2022-08-25 13:24:20 +0300

    create-question page

--- src/app/core/pages/create-question/components/question-card-constructor/question-card-constructor.component.ts
+++ src/app/core/pages/create-question/components/question-card-constructor/question-card-constructor.component.ts
@@ -1,9 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { path } from '../../../../constants/path.constant';
 import { QuestionType } from '../../../../enums/question-type.enum';
 import { IQuestionWithSingleOptionType } from '../../../../models/question.model';
 import { QuestionService } from '../../../../services/question.service';
+import { getRequiredErrorMsg } from '../../../../utils/validation.util';
 
 @Component({
   selector: 'app-question-card-constructor',
@@ -11,22 +19,67 @@ import { QuestionService } from '../../../../services/question.service';
   styleUrls: ['./question-card-constructor.component.scss'],
 })
 export class QuestionCardConstructorComponent implements OnInit {
+  form: FormGroup;
   QuestionType = QuestionType;
-  selectedType: QuestionType = QuestionType.SINGLE_CHOICE;
+  QuestionTypeArr = Object.values(QuestionType);
 
   constructor(
+    private fb: FormBuilder,
     private questionService: QuestionService,
     private router: Router
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      selectedType: [QuestionType.SINGLE_CHOICE],
+      questionText: ['', [Validators.required]],
+      choicesOfAnswers: this.fb.array([['answer1', Validators.required]]),
+    });
+  }
 
-  createQuestion() {
+  public get selectedTypeFormControl(): FormControl {
+    return this.form.get('selectedType') as FormControl;
+  }
+
+  public get questionTextFormControl(): FormControl {
+    return this.form.get('questionText') as FormControl;
+  }
+
+  public get choicesOfAnswersFormArray(): FormArray {
+    return this.form.get('choicesOfAnswers') as FormArray;
+  }
+
+  public get questionTextErrorMsg(): string {
+    if (this.questionTextFormControl.hasError('required')) {
+      return getRequiredErrorMsg('question text');
+    }
+    return '';
+  }
+
+  removeChoiceOfAnswer(index: number) {
+    console.log(index);
+    this.choicesOfAnswersFormArray.removeAt(index);
+  }
+
+  trackByFn(index: number, item: any) {
+    return index;
+  }
+
+  addChoiceOfAnswer() {
+    const choicesOfAnswersArrayLenght =
+      this.choicesOfAnswersFormArray.value.length;
+    const indexOfAnswer = choicesOfAnswersArrayLenght + 1;
+    this.choicesOfAnswersFormArray.push(
+      new FormControl('answer' + `${indexOfAnswer}`, Validators.required)
+    );
+  }
+
+  onSubmit() {
     const question: IQuestionWithSingleOptionType = {
       creationDate: new Date(),
-      type: QuestionType.SINGLE_CHOICE,
-      text: 'Дано трикутник АВС. ВD – бісектриса трикутника, ∠BAC = 60°, ∠CBD = 35°',
-      choicesOfAnswers: ['First choice', 'Second choice'],
+      type: this.selectedTypeFormControl.value,
+      text: this.questionTextFormControl.value,
+      choicesOfAnswers: this.choicesOfAnswersFormArray.value,
     };
     this.questionService.addQuestion(question);
     this.router.navigate([`/${path.QUESTION_MANAGEMENT}`]);
--- src/app/shared/components/material-ui/material-ui.module.ts
+++ src/app/shared/components/material-ui/material-ui.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -13,6 +15,8 @@ const modules = [
   MatRadioModule,
   MatInputModule,
   MatFormFieldModule,
+  MatCheckboxModule,
+  MatIconModule,
 ];
 
 @NgModule({
